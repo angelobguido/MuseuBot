10dcd0b85969a3a0ec2657d71cb14aecbf1ae23f
angelobguido <angelobguido@gmail.com>
2023-06-25 23:13:04 -0300

    Initial commit

--- app.py
+++ app.py
@@ -1,119 +1,34 @@
 import os
 from key import open_ai_key
 import streamlit as st
-from langchain.llms import OpenAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.chains import AnalyzeDocumentChain
-from langchain.prompts import PromptTemplate
-# from langchain.chains import LLMChain
-import requests
-import pandas
-import numpy as np
-import jellyfish as jf
-import math
-import re
+import museum_search
+from langchain.agents import AgentType, initialize_agent, Tool
+from langchain.chat_models import ChatOpenAI
 
-df = pandas.read_csv("peças.txt")
-df = df[["Modelo", "Responsável pela Pesquisa", "Tipo do Objeto", "Descrição do Objeto", "Função/Uso",
-         "Ano de Fabricação/Época", "Fabricante", "Local de Fabricação/Origem", "Imagens do Objeto"]]
-df = df[df["Modelo"].notna()]
-df = df[df["Descrição do Objeto"].notna()]
-df.reset_index(inplace=True)
+os.environ['OPENAI_API_KEY'] = open_ai_key
 
-url = "http://143.107.183.188/media/collectiveaccess/images/2/6/78416_ca_attribute_values_value_blob_2655_large.jpg"
-response = requests.get(url)
+tools = [
+    Tool(
+        func=museum_search.piece_tool,
+        name="Piece Search",
+        description="útil para encontrar informações sobre uma peça do museu, inserir o nome da peça ou modelo"
+    ),
+    Tool(
+        func=museum_search.type_tool,
+        name="Type Search",
+        description="útil para encontrar todas as peças do museu de um determinado tipo, inserir apenas o tipo"
+    )
+]
 
-# with open("image.jpg", "wb") as f:
-#     f.write(response.content)
-
-
-# Funcoes
-
-operacao = "tipo"
-
-
-def pesquisa(entrada: str):
-    lst = []
-    pesquisa = entrada.split()
-
-    if pesquisa[0] == '0':
-        pass
-
-    if pesquisa[0] == '1':
-        entrada = entrada[2:]
-        lst.extend(pesquisa_peca(entrada))
-        pass
-    elif pesquisa[0] == '2':
-        entrada = entrada[2:]
-        lst.extend(pesquisa_tipo(entrada))
-        pass
-
-    return lst
-
-
-def pesquisa_peca(entrada: str):
-    res = []
-    for index, value in df["Modelo"].items():
-        if jf.levenshtein_distance(entrada.lower(), str(value).lower()) <= 0.5 * len(entrada):
-            res.append(f"{jf.levenshtein_distance(entrada.lower(), str(value).lower())} {index}")
-            pass
-
-    res.sort()
-    for i in range(len(res)):
-        aux = res[i].split()
-        res[i] = int(aux[1])
-    global operacao
-    operacao = "peca"
-    return res
-
-
-def pesquisa_tipo(entrada: str):
-    res = []
-    for index, value in df["Tipo do Objeto"].items():
-        if jf.levenshtein_distance(entrada.lower(), str(value).lower()) <= 0.4 * len(entrada):
-            res.append(index)
-            pass
-    global operacao
-    operacao = "tipo"
-    return res
-
-
-def colhe_informacao(lista, operacao):
-    res = []
-
-    if operacao == "peca":
-        for indexes in lista:
-            res.append([f"Modelo: {df.iloc[indexes]['Modelo']}\n",
-                        f"Tipo do Objeto: {df.iloc[indexes]['Tipo do Objeto']}\n",
-                        f"Descrição do Objeto: {df.iloc[indexes]['Descrição do Objeto']}\n",
-                        f"Ano de Fabricação/Época: {df.iloc[indexes]['Ano de Fabricação/Época']}\n",
-                        f"Fabricante: {df.iloc[indexes]['Fabricante']}\n",
-                        f"Local de Fabricação/Origem: {df.iloc[indexes]['Local de Fabricação/Origem']}\n"],
-                       )
-            res.append(f"Imagens do Objeto: {df.iloc[indexes]['Imagens do Objeto']}\n")
-
-    if operacao == "tipo":
-        for indexes in lista:
-            res.append(f"{df.iloc[indexes]['Modelo']}")
-
-    return res
-
+agent = initialize_agent(tools, ChatOpenAI(temperature=0.5), agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 
 # app framework
 st.title('📚🤖 ChatBot do Museu')
 prompt = st.text_input('Faça sua pergunta aqui sobre o museu e suas peças:')
 
 if prompt:
-    lista = pesquisa(prompt)
-    if operacao == "peca":
-        for i in range(len(colhe_informacao(lista, operacao))//2):
-            aux = colhe_informacao(lista, operacao)[2*i]
-            for item in aux:
-                st.write(item)
-            st.markdown(colhe_informacao(lista, operacao)[2*i+1], unsafe_allow_html=True)
-    else:
-        for item in colhe_informacao(lista, operacao):
-            st.write(item)
+    #st.write(agent.run(prompt))
+    museum_search.piece_tool(prompt)
 
 else:
     st.write("""
---
+++ museum_search.py
@@ -0,0 +1,66 @@
+import pandas
+import requests
+import jellyfish as jf
+
+df = pandas.read_csv("peças.txt")
+df = df[["Modelo", "Responsável pela Pesquisa", "Tipo do Objeto", "Descrição do Objeto", "Função/Uso",
+         "Ano de Fabricação/Época", "Fabricante", "Local de Fabricação/Origem", "Imagens do Objeto"]]
+df = df[df["Modelo"].notna()]
+df = df[df["Descrição do Objeto"].notna()]
+df.reset_index(inplace=True)
+
+#Return museum pieces indexes with names similar to input
+def piece(input: str):
+    res = []
+    for index, value in df["Modelo"].items():
+        if jf.levenshtein_distance(input.lower(), str(value).lower()) <= 0.5 * len(input):
+            res.append((jf.levenshtein_distance(input.lower(), str(value).lower()), index))
+
+    res.sort()
+    res = list(map(lambda x : x[1], res))
+
+    return res
+
+#Return museum pieces indexes with types similar to input
+def type(input: str):
+    res = []
+    for index, value in df["Tipo do Objeto"].items():
+        if jf.levenshtein_distance(input.lower(), str(value).lower()) <= 0.5 * len(input):
+            res.append(index)
+
+    return res
+
+#Return museum piece information to use as an agent tool
+def piece_tool(input: str):
+    res = piece(input)
+
+    if (res.count == 0):
+        return ""
+
+    out = f'''
+    
+    Nome: {df.iloc[res[0], "Modelo"]}
+    Ano de Fabricação/Época: {df.iloc[res[0], "Ano de Fabricação/Época"]}
+    Tipo do Objeto: {df.iloc[res[0], "Tipo do Objeto"]}
+    Descrição: {df.iloc[res[0], "Descrição do Objeto"]}
+
+    Peças com nomes semelhantes: {str([df.iloc[res[x], "Modelo"] for x in res[1:]])}
+    
+    '''
+
+    return out
+
+#Return museum pieces names according to type
+def type_tool(input: str):
+    res = type(input)
+
+    if (res.count == 0):
+        return ""
+
+    out = f'''
+    
+    Peças do tipo escolhido: {str([df.iloc[res[x], "Modelo"] for x in res[1:]])}
+    
+    '''
+
+    return out
